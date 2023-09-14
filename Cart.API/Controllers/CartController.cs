53077b29b1879d8355ee8f0424543d6ff2c2589b
Vimal Gupta <vimal02352@yahoo.co.in>
2023-09-14 18:05:06 +0530

    initial commit

---
+++ Cart.API/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        [Route("cart/add")]
+        public IActionResult Add(int skuID, int quantiry, int cartID)
+        {
+            return Ok(true);
+        }
+    }
+}
---
+++ Inventory.API/Controllers/InventoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("inventory/check")]
+        public IActionResult IsProductAvailable(int skuID, int quantiry)
+        {
+
+            return Ok(true);
+        }
+    }
+}
